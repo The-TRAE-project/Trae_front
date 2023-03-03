f344eb38835881279f12ab5e25819826a6987004
sapar6ek0v <razetka0309@gmail.com>
2023-03-03 13:14:52 +0300

    add prod mode url

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACK_API_URL,
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_BACK_PROD_API_URL
+    : import.meta.env.VITE_BACK_PROD_API_URL,
 });
 
 export default instance;
