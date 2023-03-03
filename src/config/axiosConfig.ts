9db36b3bd959ee1318dce2f6960041470741e355
sapar6ek0v <razetka0309@gmail.com>
2023-03-03 13:20:34 +0300

    add dev url

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: import.meta.env.PROD
     ? import.meta.env.VITE_BACK_PROD_API_URL
-    : import.meta.env.VITE_BACK_PROD_API_URL,
+    : import.meta.env.VITE_BACK_DEV_API_URL,
 });
 
 export default instance;
