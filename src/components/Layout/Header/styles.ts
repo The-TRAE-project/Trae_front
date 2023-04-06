0a799ffbc617085a454083f35adb1f4e4b8142c9
sapar6ek0v <razetka0309@gmail.com>
2023-04-06 20:13:49 +0300

    fixed navbar horizontal line issue

--- src/components/Layout/Header/styles.ts
+++ src/components/Layout/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Wrapper = styled.header`
   position: fixed;
@@ -21,74 +21,39 @@ export const List = styled.ul`
   ${({ theme }) => theme.mixins.center};
 `;
 
-type ActiveItemProps = {
-  active: boolean;
-  hideBorder?: boolean;
-  index?: number;
-};
-
 export const Item = styled.li`
   position: relative;
-
-  &::after,
-  &::before {
-    content: '';
-    position: absolute;
-    top: 40px;
-    display: none;
-    width: 52px;
-    border: 1px solid var(--orange);
-    transform: rotate(90deg);
-    transition: var(--transition);
-    opacity: ${(props: ActiveItemProps) => (props.active ? '0' : '1')};
-  }
-
-  &:nth-child(1),
-  &:nth-child(2),
-  &:nth-child(3),
-  &:nth-child(4),
-  &:nth-child(5) {
-    &::after {
-      right: -24px;
-      display: ${(props: ActiveItemProps) =>
-        props.hideBorder ? 'none' : 'block'};
-    }
-  }
-
-  ${(props: ActiveItemProps) =>
-    props.index
-      ? css`
-          &:nth-child(${(prop: ActiveItemProps) => prop.index}) {
-            &::after {
-              display: none;
-            }
-          }
-        `
-      : css`
-          &::after {
-            display: block;
-          }
-        `}
-
-  &:nth-child(${(props: ActiveItemProps) => props.index}) {
-    &::after {
-      display: none;
-    }
-  }
 `;
+
+export const HorizontalDivider = styled.span`
+  position: absolute;
+  right: -24px;
+  top: 40px;
+  display: inline-block;
+  width: 52px;
+  height: 2px;
+  border: 1px solid var(--orange);
+  transform: rotate(90deg);
+  transition: var(--transition);
+  opacity: 1;
+`;
+
+type ButtonProps = {
+  active: boolean;
+};
 
 export const Button = styled.button`
   height: 88px;
   ${({ theme }) => theme.mixins.fCenter};
   padding: 25px 34px 28px;
   border: none;
-  border-radius: ${(props: ActiveItemProps) =>
+  border-radius: ${(props: ButtonProps) =>
     props.active ? '0px 0px 15px 15px' : '0'};
-  background-color: ${(props: ActiveItemProps) =>
+  background-color: ${(props: ButtonProps) =>
     props.active ? 'var(--orange)' : 'var(--white)'};
   font-weight: 600;
   ${({ theme }) => theme.mixins.fontSize30};
-  color: ${(props: ActiveItemProps) =>
+  color: ${(props: ButtonProps) =>
     props.active ? 'var(--white)' : 'var(--white-black)'};
   transition: var(--transition);
 `;
