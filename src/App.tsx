ca79694eaad30fe1f038047c7a98fc26119d3f60
sapar6ek0v <razetka0309@gmail.com>
2023-04-24 10:19:57 +0300

    add lazy loading to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import Layout from './components/Layout';
-import ProtectedRoute from './components/ProtectedRoute';
 import { Paths } from './constants/paths';
-
-import EmployeeLogin from './pages/EmployeeLogin';
-import EmployeeMain from './pages/EmployeeMain';
-import EmployeeProjects from './pages/EmployeeProjects';
-import EmployeeProjectStages from './pages/EmployeeProjectStages';
-import EmployeeStagesInWork from './pages/EmployeeStagesInWork';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Constructors from './pages/Constructors';
-import CreateConstructor from './pages/CreateConstructor';
-import UpdateUser from './pages/UpdateUser';
-import Employees from './pages/Employees';
-import CreateEmployee from './pages/CreateEmployee';
-import WorkTypes from './pages/WorkTypes';
-import UpdateWorkType from './pages/UpdateWorkType';
-import CreateWorkType from './pages/CreateWorkType';
-import UpdateEmployee from './pages/UpdateEmployee';
-import PersonalCabinet from './pages/PersonalCabinet';
-import PersonalCabinetEditing from './pages/PersonalCabinetEditing';
-import PersonalCabinetChangePassword from './pages/PersonalCabinetChangePassword';
-import CreateProject from './pages/CreateProject';
-import ProjectDetails from './pages/ProjectDetails';
-
 import { useAppSelector } from './helpers/hooks/useAppSelector';
 import { Roles } from './store/slices/auth/types';
+import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './components/Layout';
+import FullPageLoader from './components/FullPageLoader';
+
+const EmployeeLogin = lazy(() => import('./pages/EmployeeLogin'));
+const EmployeeMain = lazy(() => import('./pages/EmployeeMain'));
+const EmployeeProjects = lazy(() => import('./pages/EmployeeProjects'));
+const EmployeeProjectStages = lazy(
+  () => import('./pages/EmployeeProjectStages')
+);
+const EmployeeStagesInWork = lazy(() => import('./pages/EmployeeStagesInWork'));
+const Login = lazy(() => import('./pages/Login'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Projects = lazy(() => import('./pages/Projects'));
+const Constructors = lazy(() => import('./pages/Constructors'));
+const CreateConstructor = lazy(() => import('./pages/CreateConstructor'));
+const UpdateUser = lazy(() => import('./pages/UpdateUser'));
+const Employees = lazy(() => import('./pages/Employees'));
+const CreateEmployee = lazy(() => import('./pages/CreateEmployee'));
+const WorkTypes = lazy(() => import('./pages/WorkTypes'));
+const UpdateWorkType = lazy(() => import('./pages/UpdateWorkType'));
+const CreateWorkType = lazy(() => import('./pages/CreateWorkType'));
+const UpdateEmployee = lazy(() => import('./pages/UpdateEmployee'));
+const PersonalCabinet = lazy(() => import('./pages/PersonalCabinet'));
+const PersonalCabinetEditing = lazy(
+  () => import('./pages/PersonalCabinetEditing')
+);
+const PersonalCabinetChangePassword = lazy(
+  () => import('./pages/PersonalCabinetChangePassword')
+);
+const CreateProject = lazy(() => import('./pages/CreateProject'));
+const ProjectDetails = lazy(() => import('./pages/ProjectDetails'));
 
 const App = () => {
   const { isLoggedIn } = useAppSelector((store) => store.employee);
@@ -36,95 +43,109 @@ const App = () => {
 
   return (
     <Layout>
-      <Routes>
-        <Route path={Paths.LOGIN} element={<Login />} />
-        <Route path="*" element={<Navigate to={Paths.LOGIN} replace />} />
+      <Suspense fallback={<FullPageLoader />}>
+        <Routes>
+          <Route path={Paths.LOGIN} element={<Login />} />
+          <Route path="*" element={<Navigate to={Paths.LOGIN} replace />} />
 
-        {/* All users */}
-        <Route
-          element={
-            <ProtectedRoute
-              isAllowed={
-                permission === Roles.ADMIN ||
-                permission === Roles.CONSTRUCTOR ||
-                permission === Roles.EMPLOYEE
-              }
-              redirectPath={Paths.LOGIN}
-            />
-          }
-        >
-          <Route path={Paths.PERSONAL_CABINET} element={<PersonalCabinet />} />
-          <Route
-            path={Paths.PERSONAL_CABINET_EDITING}
-            element={<PersonalCabinetEditing />}
-          />
-          <Route
-            path={Paths.PERSONAL_CABINET_CHANGE_PASSWORD}
-            element={<PersonalCabinetChangePassword />}
-          />
-        </Route>
-
-        {/* Admin routes */}
-        <Route
-          element={
-            <ProtectedRoute
-              isAllowed={permission === Roles.ADMIN}
-              redirectPath={Paths.LOGIN}
-            />
-          }
-        >
-          <Route path={Paths.DASHBOARD} element={<Dashboard />} />
-          <Route path={Paths.PROJECTS} element={<Projects />} />
-          <Route path={Paths.PROJECTS_CREATE} element={<CreateProject />} />
-          <Route path={Paths.PROJECT_DETAILS} element={<ProjectDetails />} />
-          <Route path={Paths.CONSTRUCTORS} element={<Constructors />} />
-          <Route
-            path={Paths.CONSTRUCTORS_CREATE}
-            element={<CreateConstructor />}
-          />
-          <Route path={Paths.CONSTRUCTORS_EDITING} element={<UpdateUser />} />
-          <Route path={Paths.EMPLOYEES} element={<Employees />} />
-          <Route path={Paths.EMPLOYEES_CREATE} element={<CreateEmployee />} />
-          <Route path={Paths.EMPLOYEES_EDITING} element={<UpdateEmployee />} />
-          <Route path={Paths.WORK_TYPES} element={<WorkTypes />} />
-          <Route path={Paths.WORK_TYPES_EDITING} element={<UpdateWorkType />} />
-          <Route path={Paths.WORK_TYPES_CREATE} element={<CreateWorkType />} />
-        </Route>
-
-        {/* Employee routes */}
-        <Route
-          element={
-            <ProtectedRoute
-              isAllowed={permission === Roles.EMPLOYEE}
-              redirectPath={Paths.LOGIN}
-            />
-          }
-        >
-          <Route path={Paths.EMPLOYEE_LOGIN} element={<EmployeeLogin />} />
+          {/* All users */}
           <Route
             element={
               <ProtectedRoute
-                isAllowed={isLoggedIn}
-                redirectPath={Paths.EMPLOYEE_LOGIN}
+                isAllowed={
+                  permission === Roles.ADMIN ||
+                  permission === Roles.CONSTRUCTOR ||
+                  permission === Roles.EMPLOYEE
+                }
+                redirectPath={Paths.LOGIN}
               />
             }
           >
-            <Route path={Paths.EMPLOYEE_MAIN} element={<EmployeeMain />} />
             <Route
-              path={Paths.EMPLOYEE_PROJECTS}
-              element={<EmployeeProjects />}
+              path={Paths.PERSONAL_CABINET}
+              element={<PersonalCabinet />}
             />
             <Route
-              path={Paths.EMPLOYEE_PROJECT_STAGES}
-              element={<EmployeeProjectStages />}
+              path={Paths.PERSONAL_CABINET_EDITING}
+              element={<PersonalCabinetEditing />}
             />
             <Route
-              path={Paths.EMPLOYEE_STAGES_IN_WORK}
-              element={<EmployeeStagesInWork />}
+              path={Paths.PERSONAL_CABINET_CHANGE_PASSWORD}
+              element={<PersonalCabinetChangePassword />}
             />
           </Route>
-        </Route>
-      </Routes>
+
+          {/* Admin routes */}
+          <Route
+            element={
+              <ProtectedRoute
+                isAllowed={permission === Roles.ADMIN}
+                redirectPath={Paths.LOGIN}
+              />
+            }
+          >
+            <Route path={Paths.DASHBOARD} element={<Dashboard />} />
+            <Route path={Paths.PROJECTS} element={<Projects />} />
+            <Route path={Paths.PROJECTS_CREATE} element={<CreateProject />} />
+            <Route path={Paths.PROJECT_DETAILS} element={<ProjectDetails />} />
+            <Route path={Paths.CONSTRUCTORS} element={<Constructors />} />
+            <Route
+              path={Paths.CONSTRUCTORS_CREATE}
+              element={<CreateConstructor />}
+            />
+            <Route path={Paths.CONSTRUCTORS_EDITING} element={<UpdateUser />} />
+            <Route path={Paths.EMPLOYEES} element={<Employees />} />
+            <Route path={Paths.EMPLOYEES_CREATE} element={<CreateEmployee />} />
+            <Route
+              path={Paths.EMPLOYEES_EDITING}
+              element={<UpdateEmployee />}
+            />
+            <Route path={Paths.WORK_TYPES} element={<WorkTypes />} />
+            <Route
+              path={Paths.WORK_TYPES_EDITING}
+              element={<UpdateWorkType />}
+            />
+            <Route
+              path={Paths.WORK_TYPES_CREATE}
+              element={<CreateWorkType />}
+            />
+          </Route>
+
+          {/* Employee routes */}
+          <Route
+            element={
+              <ProtectedRoute
+                isAllowed={permission === Roles.EMPLOYEE}
+                redirectPath={Paths.LOGIN}
+              />
+            }
+          >
+            <Route path={Paths.EMPLOYEE_LOGIN} element={<EmployeeLogin />} />
+            <Route
+              element={
+                <ProtectedRoute
+                  isAllowed={isLoggedIn}
+                  redirectPath={Paths.EMPLOYEE_LOGIN}
+                />
+              }
+            >
+              <Route path={Paths.EMPLOYEE_MAIN} element={<EmployeeMain />} />
+              <Route
+                path={Paths.EMPLOYEE_PROJECTS}
+                element={<EmployeeProjects />}
+              />
+              <Route
+                path={Paths.EMPLOYEE_PROJECT_STAGES}
+                element={<EmployeeProjectStages />}
+              />
+              <Route
+                path={Paths.EMPLOYEE_STAGES_IN_WORK}
+                element={<EmployeeStagesInWork />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Layout>
   );
 };
---
+++ src/components/FullPageLoader/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  position: relative;
+  background: linear-gradient(
+    264.53deg,
+    var(--gradient-green1) 2.46%,
+    var(--gradient-green2) 70.65%
+  );
+`;
