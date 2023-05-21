c16c3b9ceb66418f881a32ef5aa80133cfd2f1ec
sapar6ek0v <razetka0309@gmail.com>
2023-05-21 16:13:56 +0300

    responsive style for "Login Page"

--- src/components/Users/LoginForm/styles.ts
+++ src/components/Users/LoginForm/styles.ts
@@ -2,20 +2,29 @@ import styled from 'styled-components';
 import { createStyles, TextInput } from '@mantine/core';
 
 export const FormWrapper = styled.form`
-  min-height: calc(100vh - 289px);
+  --pt: clamp(4.06rem, calc(0.59rem + 4.07vw), 5.63rem);
+  --gap: clamp(1.88rem, calc(1.04rem + 0.98vw), 2.25rem);
+
   ${({ theme }) => theme.mixins.column};
   align-items: flex-start;
-  gap: 36px;
-  padding: 90px 0px 0px;
+  gap: var(--gap);
+
+  @media ${({ theme }) => theme.bp.bpLarge} {
+  }
+
+  @media ${({ theme }) => theme.bp.bpXlarge} {
+    min-height: calc(100vh - 289px);
+    padding: var(--pt) 0px 0px;
+  }
 `;
 
 export const Input = styled(TextInput)`
   input {
-    width: 400px;
-    min-height: 73px;
+    width: var(--lgn-form-input-wd);
+    min-height: var(--lgn-form-input-ht);
     background: var(--white);
     border-radius: var(--border-radius);
-    padding: 12px 20px;
+    padding: 12px var(--lgn-form-input-pd);
     font-family: var(--font-roboto);
     ${({ theme }) => theme.mixins.fontSize28};
     font-weight: 500;
@@ -36,17 +45,17 @@ export const Input = styled(TextInput)`
 export const useInputStyles = createStyles(() => ({
   error: {
     fontFamily: 'var(--font-roboto)',
-    maxWidth: 400,
-    fontSize: 24,
+    maxWidth: 'var(--lgn-form-input-wd)',
+    fontSize: 'var(--fs-24-label)',
     fontWeight: 500,
-    lineHeight: '26px',
+    lineHeight: 'var(--fs-24-label-lh)',
     wordBreak: 'break-word',
     color: 'var(--red)',
-    letterSpacing: '1px',
+    letterSpacing: 'clamp(0.05rem, calc(0.02rem + 0.03vw), 0.06rem)',
   },
 
   root: {
-    minHeight: 73,
+    minHeight: 'var(--lgn-form-input-ht)',
   },
 
   wrapper: {
@@ -55,10 +64,10 @@ export const useInputStyles = createStyles(() => ({
 
   innerInput: {
     height: '100%',
-    padding: '12px 20px',
+    padding: '12px var(--lgn-form-input-pd)',
     fontFamily: 'var(--font-roboto)',
-    fontSize: 28,
-    lineHeight: '33px',
+    fontSize: 'var(--fs-28)',
+    lineHeight: 'var(--fs-28-lh)',
     fontWeight: 500,
     color: 'var(--black)',
 
@@ -73,19 +82,19 @@ export const useInputStyles = createStyles(() => ({
   },
 
   label: {
-    fontSize: 24,
+    fontSize: 'var(--fs-24-label)',
     fontWeight: 400,
-    lineHeight: '28px',
+    lineHeight: 'var(--fs-24-label-lh)',
     letterSpacing: '0em',
     textAlign: 'left',
     color: 'var(--white)',
-    paddingLeft: 13,
-    marginBottom: 13,
+    paddingLeft: 'var(--lgn-form-label-pm)',
+    marginBottom: 'var(--lgn-form-label-pm)',
   },
 
   input: {
-    width: 400,
-    minHeight: 73,
+    width: 'var(--lgn-form-input-wd)',
+    minHeight: 'var(--lgn-form-input-ht)',
     background: 'var(--white)',
     borderRadius: 'var(--border-radius)',
 
@@ -96,25 +105,27 @@ export const useInputStyles = createStyles(() => ({
   },
 
   rightSection: {
-    width: '4.5rem',
+    width: 'clamp(3.75rem, calc(2.09rem + 1.95vw), 4.50rem)',
 
     button: {
-      width: 35,
-      height: 35,
-      minWidth: 35,
-      minHeight: 35,
+      width: 'var(--right-icon-wh)',
+      height: 'var(--right-icon-wh)',
+      minWidth: 'var(--right-icon-wh)',
+      minHeight: 'var(--right-icon-wh)',
 
       svg: {
-        width: 35,
-        height: 35,
+        width: 'var(--right-icon-wh)',
+        height: 'var(--right-icon-wh)',
       },
     },
   },
 }));
 
 export const Button = styled.button`
-  width: 400px;
-  height: 90px;
+  --btn-ht: clamp(4.81rem, calc(3.01rem + 2.11vw), 5.63rem);
+
+  width: var(--lgn-form-input-wd);
+  height: var(--btn-ht);
   margin-top: 10px;
   ${({ theme }) => theme.mixins.fCenter};
   border: none;
@@ -125,7 +136,7 @@ export const Button = styled.button`
   text-align: center;
   color: var(--white);
 
-  &:is(:focus) {
+  &:is(:focus, :hover, :active) {
     outline: none;
   }
 `;
