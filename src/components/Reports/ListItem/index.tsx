d6d8f7a29ba622f287b35e4a7969af5d3d6fd5e0
d6felix <d6felix@gmail.com>
2023-07-28 13:05:24 +0300

    disabled old reports

--- src/components/Reports/ListItem/index.tsx
+++ src/components/Reports/ListItem/index.tsx
@@ -31,7 +31,7 @@ const ListItem = () => {
           <LinkBtn
             key={item.id}
             onClick={() => navigate(item.path)}
-            disabled={item.id === '3'}
+            disabled
             type="button"
           >
             {item.title}
