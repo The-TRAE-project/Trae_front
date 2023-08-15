c80115f8e9371e84783a46734f44ebb2fb70dc20
d6felix <d6felix@gmail.com>
2023-08-15 22:43:56 +0300

    optimization

--- src/components/Reports/ByEmployees/helpers/getDatesBetween.ts
+++ src/components/Reports/ByEmployees/helpers/getDatesBetween.ts
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs';
 
-// TODO: optimization
 export const getDatesBetween = (startDate: Date, endDate: Date) => {
-  const currentDate = new Date(startDate.getTime());
+  let currentDate = dayjs(startDate);
+  const lastDate = dayjs(endDate);
   const dates = [];
-  while (currentDate <= endDate) {
-    dates.push(dayjs(new Date(currentDate)).format('YYYY-MM-DD'));
-    currentDate.setDate(currentDate.getDate() + 1);
+  while (currentDate.isBefore(lastDate)) {
+    dates.push(currentDate.format('YYYY-MM-DD'));
+    currentDate = currentDate.add(1, 'day');
   }
   return dates;
 };
