de42ba6976a7e1cdc105aede01adbd7dffd9ec7a
d6felix <d6felix@gmail.com>
2023-09-03 13:40:44 +0300

    fixed theme sizes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,14 +2,14 @@ import mixins from './mixins';
 
 const theme = {
   bp: {
-    bpTinyS: `(min-width: 320px)`,
-    bpTinyL: `(min-width: 480px)`,
-    bpExtraSmall: `(min-width: 576px)`,
-    bpSmall: `(min-width: 48em)`, // 768px
-    bpMedium: `(min-width: 64em)`, // 1024px
-    bpLarge: `(min-width: 85.375em)`, // 1366px
-    bpXlarge: `(min-width: 120em)`, // 1920px
-    bpXxlarge: `(min-width: 160em)`,
+    bpTinyS: `(max-width: 320px)`,
+    bpTinyL: `(max-width: 480px)`,
+    bpExtraSmall: `(max-width: 576px)`,
+    bpSmall: `(max-width: 48em)`, // 768px
+    bpMedium: `(max-width: 64em)`, // 1024px
+    bpLarge: `(max-width: 85.375em)`, // 1366px
+    bpXlarge: `(max-width: 120em)`, // 1920px
+    bpXxlarge: `(max-width: 160em)`,
   },
 
   mixins,
