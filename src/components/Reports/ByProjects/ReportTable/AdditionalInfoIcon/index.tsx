c0209fd54a76a8f7893fbd126f62a35a19396e49
d6felix <d6felix@gmail.com>
2023-09-15 10:49:59 +0300

    added icon for overlapping operations

---
+++ src/components/Reports/ByProjects/ReportTable/AdditionalInfoIcon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IconInfo } from './styles';
+import { AdditionalInfo } from '../../../../svgs/AdditionalInfo';
+
+export const AdditionalInfoIcon = () => {
+  return (
+    <IconInfo>
+      <AdditionalInfo />
+    </IconInfo>
+  );
+};
---
+++ src/components/Reports/ByProjects/ReportTable/AdditionalInfoIcon/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const IconInfo = styled.div`
+  background-color: var(--white);
+  border: 2px solid var(--green);
+  height: 29px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 27px;
+`;
