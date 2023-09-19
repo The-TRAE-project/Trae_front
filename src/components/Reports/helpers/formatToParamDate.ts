225b973e5f591ae80160fad253005558c9e162dc
d6felix <d6felix@gmail.com>
2023-09-19 11:49:12 +0300

    add handle undefined

--- src/components/Reports/helpers/formatToParamDate.ts
+++ src/components/Reports/helpers/formatToParamDate.ts
@@ -3,5 +3,10 @@ import dayjs from 'dayjs';
 export const DATE_30_AHEAD = dayjs(new Date()).add(30, 'days').toDate();
 export const DATE_1_AHEAD = dayjs(new Date()).add(1, 'days').toDate();
 
-export const formatToQueryParamDate = (date: Date | string) =>
-  dayjs(date).format('YYYY-MM-DD');
+export const formatToQueryParamDate = (date: Date | string | undefined) => {
+  if (date === undefined) {
+    return '';
+  }
+
+  return dayjs(date).format('YYYY-MM-DD');
+};
